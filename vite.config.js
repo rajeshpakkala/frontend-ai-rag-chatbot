56a4bb988407d2552091b8483e29ffa7c54240ba
rajeshpakkala <rajeshpakkala786@gmail.com>
2026-08-29 19:51:02 +0530

    prod

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/chatbot': {
-        target: 'http://localhost:8080',
+        target: 'https://ai-rag-chatbot-ixdt.onrender.com',
         changeOrigin: true,
+        secure: true,
       },
     },
   },
